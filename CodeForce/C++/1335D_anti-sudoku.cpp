643d470c590c3111f45f9b2adcf126346e87df98
leejaeseung <wasd222@naver.com>
2020-04-16 17:42:51 +0900

    BOJ

---
+++ CodeForce/C++/1335D_anti-sudoku.cpp
@@ -0,0 +1,85 @@
+﻿/*
+D. Anti-Sudoku
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You are given a correct solution of the sudoku puzzle. If you don't know what is the sudoku, you can read about it here.
+
+The picture showing the correct sudoku solution:
+
+
+
+Blocks are bordered with bold black color.
+
+Your task is to change at most 9 elements of this field (i.e. choose some 1≤i,j≤9 and change the number at the position (i,j) to any other number in range [1;9]) to make it anti-sudoku. The anti-sudoku is the 9×9 field, in which:
+
+Any number in this field is in range [1;9];
+each row contains at least two equal elements;
+each column contains at least two equal elements;
+each 3×3 block (you can read what is the block in the link above) contains at least two equal elements.
+It is guaranteed that the answer exists.
+
+You have to answer t independent test cases.
+
+Input
+The first line of the input contains one integer t (1≤t≤104) — the number of test cases. Then t test cases follow.
+
+Each test case consists of 9 lines, each line consists of 9 characters from 1 to 9 without any whitespaces — the correct solution of the sudoku puzzle.
+
+Output
+For each test case, print the answer — the initial field with at most 9 changed elements so that the obtained field is anti-sudoku. If there are several solutions, you can print any. It is guaranteed that the answer exists.
+
+풀이:
+이유는 정확히 모르지만 2를 1로 바꿔주면 되는 문제였습니다..
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<string.h>
+#include<queue>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define ll long long
+
+int map[9][9];
+
+int main(void) {
+	FIO;
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		for (int i = 0; i < 9; i++)
+		{
+			string str;
+			cin >> str;
+			for (int j = 0; j < 9; j++)
+			{
+				map[i][j] = str[j] - '0';
+			}
+		}
+		
+
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					if (map[i][j] == 2)
+						map[i][j] = 1;
+				}
+			}
+
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				cout << map[i][j];
+			}
+			cout << "\n";
+		}
+	}
+}
---
+++ CodeForce/C++/455A_Boredom.cpp
@@ -0,0 +1,74 @@
+﻿/*
+A. Boredom
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Alex doesn't like boredom. That's why whenever he gets bored, he comes up with games. One long winter evening he came up with a game and decided to play it.
+
+Given a sequence a consisting of n integers. The player can make several steps. In a single step he can choose an element of the sequence (let's denote it ak) and delete it, at that all elements equal to ak + 1 and ak - 1 also must be deleted from the sequence. That step brings ak points to the player.
+
+Alex is a perfectionist, so he decided to get as many points as possible. Help him.
+
+Input
+The first line contains integer n (1 ≤ n ≤ 105) that shows how many numbers are in Alex's sequence.
+
+The second line contains n integers a1, a2, ..., an (1 ≤ ai ≤ 105).
+
+Output
+Print a single integer — the maximum number of points that Alex can earn.
+
+풀이:
+dp[i][0 or 1] -> 0 일 때 = i 를 선택하지 않을 경우의 최댓값, 1 일 때 = i 를 선택할 경우의 최댓값으로 정의합니다.
+
+문제에 사용된 모든 숫자를 저장하고, 정렬합니다. 그리고 각 숫자마다 몇 개인지 카운트를 세줍니다.
+
+1. i번째를 선택하지 않을 때(dp[i][0]) = i - 1 번째를 선택할 때 와 i - 1 번째를 선택하지 않을 때 중 큰 값
+2. i번째를 선택할 때(dp[i][1]) = 
+2-1. i번째 숫자와 i - 1 번째 숫자의 차이가 1 보다 클 때 = i - 1 번째를 선택하고, i 번째 숫자의 개수만큼 더함.
+2-2. i번째 숫자와 i - 1 번째 숫자의 차이가 1 보다 작을 때 = i - 1 번째를 선택하지 않고, i 번째 숫자의 개수만큼 더함.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<string.h>
+#include<vector>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+
+int N;
+long long cnt[100002];
+long long dp[100002][2];
+vector<long long> vt;
+
+int main(void) {
+	FIO;
+
+	cin >> N;
+
+	for (int i = 1; i <= N ; i++)
+	{
+		int v;
+		cin >> v;
+		if (cnt[v] == 0)
+			vt.push_back(v);
+		cnt[v]++;
+	}
+
+	sort(vt.begin(), vt.end());
+
+	dp[0][1] = vt[0] * cnt[vt[0]];
+	for (int i = 1; i < vt.size(); i++)
+	{
+		long long n = vt[i];
+		long long c = cnt[n];
+
+		dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+		dp[i][1] = dp[i - 1][0] + n * c;
+		long long temp = dp[i - 1][1] + n * c;
+		if (abs(n - vt[i - 1]) > 1 && temp > dp[i][1])	dp[i][1] = temp;
+	}
+	cout << max(dp[vt.size() - 1][0], dp[vt.size() - 1][1]);
+}
