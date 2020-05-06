01de6b5d41e92e8a72abb841ec2ab362f883bb19
leejaeseung <wasd222@naver.com>
2020-05-06 16:40:46 +0900

    BOJ

---
+++ CodeForce/C++/1324F_Maximum White Subtree.cpp
@@ -0,0 +1,128 @@
+﻿/*
+F. Maximum White Subtree
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You are given a tree consisting of n vertices. A tree is a connected undirected graph with n−1 edges. Each vertex v of this tree has a color assigned to it (av=1 if the vertex v is white and 0 if the vertex v is black).
+
+You have to solve the following problem for each vertex v: what is the maximum difference between the number of white and the number of black vertices you can obtain if you choose some subtree of the given tree that contains the vertex v? The subtree of the tree is the connected subgraph of the given tree. More formally, if you choose the subtree that contains cntw white vertices and cntb black vertices, you have to maximize cntw−cntb.
+
+Input
+The first line of the input contains one integer n (2≤n≤2⋅105) — the number of vertices in the tree.
+
+The second line of the input contains n integers a1,a2,…,an (0≤ai≤1), where ai is the color of the i-th vertex.
+
+Each of the next n−1 lines describes an edge of the tree. Edge i is denoted by two integers ui and vi, the labels of vertices it connects (1≤ui,vi≤n,ui≠vi).
+
+It is guaranteed that the given edges form a tree.
+
+Output
+Print n integers res1,res2,…,resn, where resi is the maximum possible difference between the number of white and black vertices in some subtree that contains the vertex i.
+
+풀이:
+dfs를 두 번하여 해결했습니다.
+첫 번째 dfs에선 각 노드마다 그 노드를 root로 했을 때 나올 수 있는 최댓값을 구해 배열에 저장합니다.
+
+두 번째 dfs에선 root노드부터 leaf노드까지 탐색하는데, 부모의 최댓값을 전파하며 내려갑니다.
+
+자식 노드의 최댓값이 0 이상일 경우 -> max(자식 노드의 최댓값, 부모 노드의 최댓값) 을 전파합니다.
+자식 노드의 최댓값이 0 보다 작을 경우 -> max(자식 노드의 최댓값 - 1, 부모 노드의 최댓값) 을 전파합니다.
+(자식 노드의 최댓값이 0보다 작다는 건 아무리 최대여도 -1을 해줘야 한다는 뜻입니다.)
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<queue>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define ll long long
+
+int N;
+int color[200001];
+vector<int> vt[200001];
+bool visit[200001];
+int maxV[200001];
+int out[200001];
+
+int dfs(int now) {
+
+	int v = color[now];
+
+	for (int i = 0; i < vt[now].size(); i++)
+	{
+		int next = vt[now][i];
+		
+		if (!visit[next]) {
+			visit[next] = true;
+			v = max(v, dfs(next) + v);
+		}
+	}
+
+	return maxV[now] = v;
+	//now 노드가 root일 때 가질 수 있는 최댓값
+}
+
+void dfs2(int now, int prevV) {
+
+	out[now] = prevV;
+	//최종 결과값을 배열에 저장
+
+	for (int i = 0; i < vt[now].size(); i++)
+	{
+		int next = vt[now][i];
+
+		if (!visit[next]) {
+
+			visit[next] = true;
+
+			if(maxV[next] >= 0)
+				dfs2(next, max(prevV, maxV[next]));
+			//자식 vs 부모 중 큰 값을 전파
+			else
+				dfs2(next, max(prevV - 1, maxV[next]));
+		}
+	}
+}
+
+int main(void) {
+	FIO;
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		int v;
+		cin >> v;
+		if (v == 0)
+			color[i] = -1;
+		else
+			color[i] = 1;
+	}
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		vt[a].push_back(b);
+		vt[b].push_back(a);
+	}
+
+	visit[1] = true;
+	dfs(1);
+	for (int i = 0; i < 200001; i++)
+	{
+		visit[i] = false;
+	}
+	visit[1] = true;
+	dfs2(1, maxV[1]);
+
+	for (int i = 1; i <= N; i++)
+	{
+		cout << out[i] << " ";
+	}
+}
---
+++ CodeForce/C++/1348C_Phoenix and Distribution.cpp
@@ -0,0 +1,93 @@
+﻿/*
+C. Phoenix and Distribution
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Phoenix has a string s consisting of lowercase Latin letters. He wants to distribute all the letters of his string into k non-empty strings a1,a2,…,ak such that every letter of s goes to exactly one of the strings ai. The strings ai do not need to be substrings of s. Phoenix can distribute letters of s and rearrange the letters within each string ai however he wants.
+
+For example, if s= baba and k=2, Phoenix may distribute the letters of his string in many ways, such as:
+
+ba and ba
+a and abb
+ab and ab
+aa and bb
+But these ways are invalid:
+
+baa and ba
+b and ba
+baba and empty string (ai should be non-empty)
+Phoenix wants to distribute the letters of his string s into k strings a1,a2,…,ak to minimize the lexicographically maximum string among them, i. e. minimize max(a1,a2,…,ak). Help him find the optimal distribution and print the minimal possible value of max(a1,a2,…,ak).
+
+String x is lexicographically less than string y if either x is a prefix of y and x≠y, or there exists an index i (1≤i≤min(|x|,|y|)) such that xi < yi and for every j (1≤j<i) xj=yj. Here |x| denotes the length of the string x.
+
+Input
+The input consists of multiple test cases. The first line contains an integer t (1≤t≤1000) — the number of test cases. Each test case consists of two lines.
+
+The first line of each test case consists of two integers n and k (1≤k≤n≤105) — the length of string s and the number of non-empty strings, into which Phoenix wants to distribute letters of s, respectively.
+
+The second line of each test case contains a string s of length n consisting only of lowercase Latin letters.
+
+It is guaranteed that the sum of n over all test cases is ≤105.
+
+Output
+Print t answers — one per test case. The i-th answer should be the minimal possible value of max(a1,a2,…,ak) in the i-th test case.
+
+풀이:
+먼저, 문자열 s 를 사전순서로 정렬합니다.
+문자열을 k개로 나눠야 하므로 정렬한 문자열 s 에서 맨 처음 문자와 K번째의 문자가 같지 않다면
+K번째 문자 하나만을 택하는게 최선의 선택입니다. -> 그 앞에 오는 문자들을 K번째 문자를 제외하고 모두 합칠 수 있습니다.
+
+맨 처음 문자와 K번째 문자가 같다면, 첫번쨰~K번째까지 모두 같은 문자라는 뜻입니다.
+따라서 모든 문자열에 첫번째 문자를 붙일 수 있습니다.
+그리고 K + 1번째 문자와 마지막 문자가 다르다면 K + 1번째 문자부터 마지막 문자까지 더해주고,
+아니라면 모든 문자가 같으므로 짝수 길이의 해당 문자가 정답이 됩니다.(가장 긴)
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define ll long long
+
+int T, N, K;
+
+int main(void) {
+	FIO;
+
+	cin >> T;
+
+	while (T--) {
+		cin >> N >> K;
+
+		string s;
+		cin >> s;
+
+		sort(s.begin(), s.end());
+
+		if (s[0] != s[K - 1]) {
+			cout << s[K - 1];
+		}
+		else {
+			cout << s[0];
+
+			if (s[K] != s[N - 1]) {
+				for (int i = K; i < N; i++)
+				{
+					cout << s[i];
+				}
+			}
+			else {
+				for (int i = 0; i < (N - 1) / K; i++)
+				{
+					cout << s[N - 1];
+				}
+			}
+		}
+		cout << "\n";
+	}
+}
