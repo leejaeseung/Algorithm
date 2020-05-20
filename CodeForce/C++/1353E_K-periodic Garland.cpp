9458a5dacd1554011f3ee45e81f266facb042b72
leejaeseung <wasd222@naver.com>
2020-05-20 16:30:46 +0900

    CodeForce #642 Div.3

---
+++ CodeForce/C++/1353E_K-periodic Garland.cpp
@@ -0,0 +1,100 @@
+﻿/*
+E. K-periodic Garland
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You are given a garland consisting of n lamps. States of the lamps are represented by the string s of length n. The i-th character of the string si equals '0' if the i-th lamp is turned off or '1' if the i-th lamp is turned on. You are also given a positive integer k.
+
+In one move, you can choose one lamp and change its state (i.e. turn it on if it is turned off and vice versa).
+
+The garland is called k-periodic if the distance between each pair of adjacent turned on lamps is exactly k. Consider the case k=3. Then garlands "00010010", "1001001", "00010" and "0" are good but garlands "00101001", "1000001" and "01001100" are not. Note that the garland is not cyclic, i.e. the first turned on lamp is not going after the last turned on lamp and vice versa.
+
+Your task is to find the minimum number of moves you need to make to obtain k-periodic garland from the given one.
+
+You have to answer t independent test cases.
+
+Input
+The first line of the input contains one integer t (1≤t≤25 000) — the number of test cases. Then t test cases follow.
+
+The first line of the test case contains two integers n and k (1≤n≤106;1≤k≤n) — the length of s and the required period. The second line of the test case contains the string s consisting of n characters '0' and '1'.
+
+It is guaranteed that the sum of n over all test cases does not exceed 106 (∑n≤106).
+
+Output
+For each test case, print the answer — the minimum number of moves you need to make to obtain k-periodic garland from the given one.
+
+풀이:
+문자열을 k 번씩 건너뛰어가며 자릅니다.
+100011010 , k = 3 이라면
+
+1 : 100
+2 : 011
+3 : 010
+과 같이 됩니다. 이제 위 부분 문자열은 모두 0이 되던가, 단 하나의 문자열만 전부 1이 되야 합니다.
+
+최종적으로 구해야 하는 값은 바꿔야하는 0 혹은 1의 개수의 최소값인데,
+그 말은 즉 이미 완성되어있는 1은 최대한 살려가며 1을 없애는 게 최적일 것입니다.
+
+위 예에선 2번째 부분 문자열을 살려서 나머지 1두 개를 지우는게 최적입니다.
+0에는 -1의 패널티를 주고 1에는 +1의 값을 준 뒤 부분 문자열에 대해 총 합을 구합니다.(단, 총 합이 0보다 작아질 경우 0 그대로입니다.)
+구해지는 총 합 중 최대값이 가장 길게 살릴 수 있는 1의 개수가 되므로, 총 1의 개수에서 구한 값을 빼주면 답이 됩니다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pii>
+#define IMAX 2000000001
+int mv1[4] = { 0, 0, 1, -1 };
+int mv2[4] = { 1, -1, 0, 0 };
+
+int t, n, k;
+string str;
+int dp[1000001];
+
+int main(void) {
+	FIO;
+
+	cin >> t;
+
+	while (t--) {
+		cin >> n >> k >> str;
+
+		int all = 0;
+		int ans = 0;
+
+		for (int i = 0; i < k; i++)
+		{
+			if (str[i] == '1')
+				all++;
+
+			ans = max(ans, dp[i] = (int)(str[i] - '0'));
+			//매번 가장 긴 1의 개수를 비교합니다.
+			for (int j = i + k; j < n; j = j + k)
+			{
+				if (str[j] == '0')
+					dp[j] = max(0, dp[j - k] - 1);
+				else {
+					dp[j] = dp[j - k] + 1;
+					all++;
+				}
+				ans = max(ans, dp[j]);
+				//매번 가장 긴 1의 개수를 비교합니다.
+			}
+		}
+
+		cout << all - ans << "\n";
+	}
+}
---
+++ CodeForce/C++/52C_Circular RMQ.cpp
@@ -0,0 +1,121 @@
+﻿/*
+C. Circular RMQ
+time limit per test1.5 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You are given circular array a 0, a 1, ..., a n - 1. There are two types of operations with it:
+
+inc(lf, rg, v) — this operation increases each element on the segment [lf, rg] (inclusively) by v;
+rmq(lf, rg) — this operation returns minimal value on the segment [lf, rg] (inclusively).
+Assume segments to be circular, so if n = 5 and lf = 3, rg = 1, it means the index sequence: 3, 4, 0, 1.
+
+Write program to process given sequence of operations.
+
+Input
+The first line contains integer n (1 ≤ n ≤ 200000). The next line contains initial state of the array: a 0, a 1, ..., a n - 1 ( - 106 ≤ a i ≤ 106), a i are integer. The third line contains integer m (0 ≤ m ≤ 200000), m — the number of operartons. Next m lines contain one operation each. If line contains two integer lf, rg (0 ≤ lf, rg ≤ n - 1) it means rmq operation, it contains three integers lf, rg, v (0 ≤ lf, rg ≤ n - 1; - 106 ≤ v ≤ 106) — inc operation.
+
+Output
+For each rmq operation write result for it. Please, do not use %lld specificator to read or write 64-bit integers in C++. It is preffered to use cout (also you may use %I64d).
+
+풀이:
+세그먼트 트리 응용 문제입니다.
+
+최소값 세그먼트 트리를 사용하는데, 구간을 증가시키는 쿼리가 있기 때문에 주의해야 합니다.
+모든 노드들은 자신의 구간이 얼마나 증가했는지를 누적해서 가지고 있게 됩니다.
+자식 구간들은 어차피 자신의 증가값만큼 증가하기 때문에 업데이트 시 더 내려갈 필요가 없습니다.
+(증가값만 갱신해 줍니다.)
+노드의 최소값은 현재 최소값 + 증가값 이 됩니다.
+lazy propagation과 비슷한 느낌?입니다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 100000000000000
+int mv1[4] = { 0, 0, 1, -1 };
+int mv2[4] = { 1, -1, 0, 0 };
+
+ll tree[900001];
+ll inc[9000001];
+int n, m;
+
+void update(int now, int s, int e, int l, int r, ll v) {
+	if (r < s || e < l)	return;
+	if (l <= s && e <= r) {
+		inc[now] += v;
+		return;
+	}
+	if (s == e) {
+		tree[now] += v;
+		return;
+	}
+
+	update(now * 2, s, (s + e) / 2, l, r, v);
+	update(now * 2 + 1, (s + e) / 2 + 1, e, l, r, v);
+	tree[now] = min(tree[now * 2] + inc[now * 2], tree[now * 2 + 1] + inc[now * 2 + 1]);
+}
+
+ll getMin(int now, int s, int e, int l, int r) {
+	if (r < s || e < l)	return LMAX;
+	if (l <= s && e <= r)	return tree[now] + inc[now];
+
+	return min(getMin(now * 2, s, (s + e) / 2, l, r), getMin(now * 2 + 1, (s + e) / 2 + 1, e, l, r)) + inc[now];
+}
+
+int main(void) {
+	FIO;
+	
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		ll v;
+		cin >> v;
+
+		update(1, 0, n - 1, i, i, v);
+	}
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		int l, r, c;
+		ll v = LMAX;
+		cin >> l >> r;
+
+		if (cin.get() != '\n')
+			cin >> v;
+
+		if (v == LMAX) {
+			if (l > r) {
+				cout << min(getMin(1, 0, n - 1, l, n - 1), getMin(1, 0, n - 1, 0, r)) << "\n";
+			}
+			else {
+				cout << getMin(1, 0, n - 1, l, r) << "\n";
+			}
+		}
+		else {
+			if (l > r) {
+				update(1, 0, n - 1, l, n - 1, v);
+				update(1, 0, n - 1, 0, r, v);
+			}
+			else {
+				update(1, 0, n - 1, l, r, v);
+			}
+		}
+	}
+}
