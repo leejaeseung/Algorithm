02638869857ec82f6f6bcfcf33b0f93d8efad200
leejaeseung <wasd222@naver.com>
2020-06-28 20:13:52 +0900

    dfs

---
+++ BOJ/C++/2668_숫자고르기.cpp
@@ -0,0 +1,90 @@
+/*
+문제
+세로 두 줄, 가로로 N개의 칸으로 이루어진 표가 있다. 첫째 줄의 각 칸에는 정수 1, 2, …, N이 차례대로 들어 있고 둘째 줄의 각 칸에는 1이상 N이하인 정수가 들어 있다. 첫째 줄에서 숫자를 적절히 뽑으면, 그 뽑힌 정수들이 이루는 집합과, 뽑힌 정수들의 바로 밑의 둘째 줄에 들어있는 정수들이 이루는 집합이 일치한다. 이러한 조건을 만족시키도록 정수들을 뽑되, 최대로 많이 뽑는 방법을 찾는 프로그램을 작성하시오. 예를 들어, N=7인 경우 아래와 같이 표가 주어졌다고 하자.
+
+
+
+이 경우에는 첫째 줄에서 1, 3, 5를 뽑는 것이 답이다. 첫째 줄의 1, 3, 5밑에는 각각 3, 1, 5가 있으며 두 집합은 일치한다. 이때 집합의 크기는 3이다. 만약 첫째 줄에서 1과 3을 뽑으면, 이들 바로 밑에는 정수 3과 1이 있으므로 두 집합이 일치한다. 그러나, 이 경우에 뽑힌 정수의 개수는 최대가 아니므로 답이 될 수 없다.
+
+입력
+첫째 줄에는 N(1≤N≤100)을 나타내는 정수 하나가 주어진다. 그 다음 줄부터는 표의 둘째 줄에 들어가는 정수들이 순서대로 한 줄에 하나씩 입력된다.
+
+출력
+첫째 줄에 뽑힌 정수들의 개수를 출력하고, 그 다음 줄부터는 뽑힌 정수들을 작은 수부터 큰 수의 순서로 한 줄에 하나씩 출력한다.
+
+풀이:
+1~N 까지의 정점을 단방향 그래프로 이은 뒤, 각 정점에서 자기 자신으로 돌아올 수 있는지 판별하는 문제입니다.
+dfs 알고리즘을 이용하면 쉽게 풀이할 수 있습니다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+int mv1[4] = { 0, 1, 0, -1 };
+int mv2[4] = { 1, 0, -1, 0 };
+
+int n;
+int link[101];
+bool visit[101];
+int cnt = 0;
+vector<int> ans;
+
+void dfs(int start, int now) {
+	
+	if (link[now] == start) {
+		cnt++;
+		ans.push_back(start);
+		return;
+	}
+	else {
+		if (!visit[link[now]]) {
+			visit[link[now]] = true;
+			dfs(start, link[now]);
+		}
+	}
+}
+
+int main(void) {
+	FIO;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int next;
+		cin >> next;
+
+		link[i] = next;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			visit[j] = false;
+		}
+		dfs(i, i);
+	}
+	cout << cnt << "\n";
+	for (int i = 0; i < cnt; i++)
+	{
+		cout << ans[i] << "\n";
+	}
+}
