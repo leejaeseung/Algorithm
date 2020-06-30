c85c87d018e7bf112e7586596b0880dd08a481ef
leejaeseung <wasd222@naver.com>
2020-06-30 13:43:26 +0900

    bfs

---
+++ BOJ/C++/5014_스타트링크.cpp
@@ -0,0 +1,83 @@
+/*
+문제
+강호는 코딩 교육을 하는 스타트업 스타트링크에 지원했다. 오늘은 강호의 면접날이다. 하지만, 늦잠을 잔 강호는 스타트링크가 있는 건물에 늦게 도착하고 말았다.
+
+스타트링크는 총 F층으로 이루어진 고층 건물에 사무실이 있고, 스타트링크가 있는 곳의 위치는 G층이다. 강호가 지금 있는 곳은 S층이고, 이제 엘리베이터를 타고 G층으로 이동하려고 한다.
+
+보통 엘리베이터에는 어떤 층으로 이동할 수 있는 버튼이 있지만, 강호가 탄 엘리베이터는 버튼이 2개밖에 없다. U버튼은 위로 U층을 가는 버튼, D버튼은 아래로 D층을 가는 버튼이다. (만약, U층 위, 또는 D층 아래에 해당하는 층이 없을 때는, 엘리베이터는 움직이지 않는다)
+
+강호가 G층에 도착하려면, 버튼을 적어도 몇 번 눌러야 하는지 구하는 프로그램을 작성하시오. 만약, 엘리베이터를 이용해서 G층에 갈 수 없다면, "use the stairs"를 출력한다.
+
+입력
+첫째 줄에 F, S, G, U, D가 주어진다. (1 ≤ S, G ≤ F ≤ 1000000, 0 ≤ U, D ≤ 1000000) 건물은 1층부터 시작하고, 가장 높은 층은 F층이다.
+
+출력
+첫째 줄에 강호가 S층에서 G층으로 가기 위해 눌러야 하는 버튼의 수의 최솟값을 출력한다. 만약, 엘리베이터로 이동할 수 없을 때는 "use the stairs"를 출력한다.
+
+풀이:
+bfs 기본 문제입니다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+int mv1[4] = { 0, 1, 0, -1 };
+int mv2[4] = { 1, 0, -1, 0 };
+
+int f, s, g, u, d;
+bool visit[1000001];
+
+int bfs() {
+	queue<pii> q;
+	q.push(make_pair(s, 0));
+	visit[s] = true;
+
+	while (!q.empty()) {
+		int now = q.front().first;
+		int now_cnt = q.front().second;
+		q.pop();
+
+		if (now == g)
+			return now_cnt;
+
+		if (now + u <= f && !visit[now + u]) {
+			visit[now + u] = true;
+			q.push(make_pair(now + u, now_cnt + 1));
+		}
+		if (now - d >= 1 && !visit[now - d]) {
+			visit[now - d] = true;
+			q.push(make_pair(now - d, now_cnt + 1));
+		}
+	}
+	return -1;
+}
+
+int main(void) {
+	FIO;
+
+	cin >> f >> s >> g >> u >> d;
+
+	int ans = bfs();
+
+	if (ans == -1)
+		cout << "use the stairs";
+	else
+		cout << ans;
+}
