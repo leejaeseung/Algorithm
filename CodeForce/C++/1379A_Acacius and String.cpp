ce6921a91f354c0228083ea8eee19650f22dea74
leejaeseung <wasd222@naver.com>
2020-07-20 15:37:15 +0900

    CodeForce

---
+++ CodeForce/C++/1379A_Acacius and String.cpp
@@ -0,0 +1,141 @@
+﻿/*
+A. Acacius and String
+time limit per test1 second
+memory limit per test512 megabytes
+inputstandard input
+outputstandard output
+Acacius is studying strings theory. Today he came with the following problem.
+
+You are given a string s of length n consisting of lowercase English letters and question marks. It is possible to replace question marks with lowercase English letters in such a way that a string "abacaba" occurs as a substring in a resulting string exactly once?
+
+Each question mark should be replaced with exactly one lowercase English letter. For example, string "a?b?c" can be transformed into strings "aabbc" and "azbzc", but can't be transformed into strings "aabc", "a?bbc" and "babbc".
+
+Occurrence of a string t of length m in the string s of length n as a substring is a index i (1≤i≤n−m+1) such that string s[i..i+m−1] consisting of m consecutive symbols of s starting from i-th equals to string t. For example string "ababa" has two occurrences of a string "aba" as a substring with i=1 and i=3, but there are no occurrences of a string "aba" in the string "acba" as a substring.
+
+Please help Acacius to check if it is possible to replace all question marks with lowercase English letters in such a way that a string "abacaba" occurs as a substring in a resulting string exactly once.
+
+Input
+First line of input contains an integer T (1≤T≤5000), number of test cases. T pairs of lines with test case descriptions follow.
+
+The first line of a test case description contains a single integer n (7≤n≤50), length of a string s.
+
+The second line of a test case description contains string s of length n consisting of lowercase English letters and question marks.
+
+Output
+For each test case output an answer for it.
+
+In case if there is no way to replace question marks in string s with a lowercase English letters in such a way that there is exactly one occurrence of a string "abacaba" in the resulting string as a substring output "No".
+
+Otherwise output "Yes" and in the next line output a resulting string consisting of n lowercase English letters. If there are multiple possible strings, output any.
+
+You may print every letter in "Yes" and "No" in any case you want (so, for example, the strings yEs, yes, Yes, and YES will all be recognized as positive answer).
+
+풀이:
+? 문자를 포함한 문자열이 주어질 때, ? 문자를 다른 문자로 바꿔서 "abacaba" 란 부분 문자열이 
+단 하나만 존재할 수 있게 할 수 있는지 묻는 문제였습니다.
+
+먼저, 문자열 s의 모든 시작점 i로부터 "abacaba"를 만들 수 있는지 조사합니다.(? 문자 포함)
+만들 수 없다면, 다음 번 i를 조사합니다.
+
+만들 수 있다면, 해당 지점을 "abacaba"로 만들어보고, 다른 ? 문자는 z 등으로 바꿉니다.(절대 "abacaba"를 만들 수 없게)
+그런 뒤 해당 문자열에 "abacaba"가 몇 개 나오는지 조사하고, 만약 1개라면 Yes를 출력합니다.
+
+Yes가 출력되지 않았다면, No를 출력합니다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+int mv1[4] = { 0, 1, 0, -1 };
+int mv2[4] = { 1, 0, -1, 0 };
+
+int t, n;
+string com = "abacaba";
+
+int count(string str) {
+	int ret = 0;
+	
+	for (int i = 0; i <= n - 7; i++)
+	{
+		bool flag = true;
+
+		for (int j = 0; j < 7; j++)
+		{
+			if (com[j] != str[i + j]) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag)
+			ret++;
+	}
+	return ret;
+}
+
+int main(void) {
+	FIO;
+
+	cin >> t;
+	
+	while (t--) {
+		string s;
+		cin >> n >> s;
+
+		int cnt = 0;
+		bool can = false;
+
+		for (int i = 0; i <= n - 7; i++)
+		{
+			string temp = s;
+
+			bool flag = true;
+			for (int j = 0; j < 7; j++)
+			{
+				if (com[j] != temp[i + j] && temp[i + j] != '?') {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				for (int j = 0; j < 7; j++)
+				{
+					temp[i + j] = com[j];
+				}
+
+				for (int j = 0; j < n; j++)
+				{
+					if (temp[j] == '?')
+						temp[j] = 'z';
+				}
+
+				if (count(temp) == 1) {
+					cout << "Yes\n" << temp << "\n";
+					can = true;
+					break;
+				}
+			}
+		}
+
+		if (!can) {
+			cout << "No\n";
+		}
+	}
+}
---
+++ CodeForce/C++/1379B_Dubious Cyrpto.cpp
@@ -0,0 +1,106 @@
+﻿/*
+B. Dubious Cyrpto
+time limit per test1 second
+memory limit per test512 megabytes
+inputstandard input
+outputstandard output
+Pasha loves to send strictly positive integers to his friends. Pasha cares about security, therefore when he wants to send an integer n, he encrypts it in the following way: he picks three integers a, b and c such that l≤a,b,c≤r, and then he computes the encrypted value m=n⋅a+b−c.
+
+Unfortunately, an adversary intercepted the values l, r and m. Is it possible to recover the original values of a, b and c from this information? More formally, you are asked to find any values of a, b and c such that
+
+a, b and c are integers,
+l≤a,b,c≤r,
+there exists a strictly positive integer n, such that n⋅a+b−c=m.
+Input
+The first line contains the only integer t (1≤t≤20) — the number of test cases. The following t lines describe one test case each.
+
+Each test case consists of three integers l, r and m (1≤l≤r≤500000, 1≤m≤1010). The numbers are such that the answer to the problem exists.
+
+Output
+For each test case output three integers a, b and c such that, l≤a,b,c≤r and there exists a strictly positive integer n such that n⋅a+b−c=m. It is guaranteed that there is at least one possible solution, and you can output any possible combination if there are multiple solutions.
+
+풀이:
+a, b, c 의 범위 l, r 이 주어지고, m 이 주어질 때, m = n * a + b - c 를 만족하는 a, b, c를 찾는 문제입니다.
+(n은 어떤 양수 값이나 될 수 있습니다. 0 제외)
+
+n이 어떤 값이든 될 수 있으므로, 결국 m을 a로 나눴을 때 나머지를 b - c로 채워줘야 하는 문제입니다.
+a를 l ~ r 중 한 값으로 두고, m을 나눠보면서, 해당 나머지를 b - c 로 만들 수 있는지 확인하면 됩니다.
+
+10 12 9 와 같은 경우, 위 식을 만족시키려면 n이 0이 될 수 없으므로, a = 10, n = 1 , 따라서 b - c 가 -1 이 되야 합니다.
+10 12 19 도 비슷하게, a = 10, n = 2, b - c = -1 입니다.
+
+b와 c 도 10 ~ 12 사이의 값이므로 b - c 만으로 나머지인 9를 만들 수 없기 때문에 n을 증가시키고, 뺴주어 9를 만든 것을 볼 수 있습니다.
+
+m 값이 int 범위를 초과하는 것에 주의합니다. ㅡㅡ
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+int mv1[4] = { 0, 1, 0, -1 };
+int mv2[4] = { 1, 0, -1, 0 };
+
+int t;
+
+int main(void) {
+	FIO;
+
+	cin >> t;
+
+	while (t--) {
+		ll l, r, m;
+
+		cin >> l >> r >> m;
+
+		ll a, b, c;
+		ll range = r - l;
+		
+		for (int i = l; i <= r; i++)
+		{
+			a = i;
+
+			ll remain = m % a;
+			remain = min(remain, a - remain);
+
+			if (remain != m % a || a > m) {
+				//나머지를 b - c 로 만들 수 없을 때
+				remain = m % a;
+				remain = a - remain;
+
+				b = l;
+				c = l + remain;
+
+				if (c >= l && c <= r) {
+					cout << a << " " << b << " " << c << "\n";
+					break;
+				}
+			}
+			else {
+				b = l + remain;
+				c = l;
+				if (b >= l && b <= r) {
+					cout << a << " " << b << " " << c << "\n";
+					break;
+				}
+			}
+		}
+	}
+}
