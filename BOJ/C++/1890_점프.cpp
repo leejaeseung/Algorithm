d23ac4946305b79e1820c876d172f691aaa9a3e0
leejaeseung <wasd222@naver.com>
2020-08-04 12:57:07 +0900

    DP, DFS

---
+++ BOJ/C++/1890_점프.cpp
@@ -0,0 +1,88 @@
+/*
+문제
+N×N 게임판에 수가 적혀져 있다. 이 게임의 목표는 가장 왼쪽 위 칸에서 가장 오른쪽 아래 칸으로 규칙에 맞게 점프를 해서 가는 것이다.
+
+각 칸에 적혀있는 수는 현재 칸에서 갈 수 있는 거리를 의미한다. 반드시 오른쪽이나 아래쪽으로만 이동해야 한다. 0은 더 이상 진행을 막는 종착점이며, 항상 현재 칸에 적혀있는 수만큼 오른쪽이나 아래로 가야 한다. 한 번 점프를 할 때, 방향을 바꾸면 안 된다. 즉, 한 칸에서 오른쪽으로 점프를 하거나, 아래로 점프를 하는 두 경우만 존재한다.
+
+가장 왼쪽 위 칸에서 가장 오른쪽 아래 칸으로 규칙에 맞게 이동할 수 있는 경로의 개수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 게임 판의 크기 N (4 ≤ N ≤ 100)이 주어진다. 그 다음 N개 줄에는 각 칸에 적혀져 있는 수가 N개씩 주어진다. 칸에 적혀있는 수는 0보다 크거나 같고, 9보다 작거나 같은 정수이며, 가장 오른쪽 아래 칸에는 항상 0이 주어진다.
+
+출력
+가장 왼쪽 위 칸에서 가장 오른쪽 아래 칸으로 문제의 규칙에 맞게 갈 수 있는 경로의 개수를 출력한다. 경로의 개수는 263-1보다 작거나 같다.
+
+풀이:
+dp를 적용한 dfs 문제이다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+int mv1[4] = { 0, 1, 0, -1 };
+int mv2[4] = { 1, 0, -1, 0 };
+int mv_all1[8] = { 0, 1, 0, -1, -1, -1, 1, 1 };
+int mv_all2[8] = { 1, 0, -1, 0 , -1, 1, -1, 1 };
+
+int n;
+int maps[101][101];
+ll dp[101][101];
+
+ll dfs(int x, int y) {
+
+	ll& ret = dp[x][y];
+
+	if (ret != -1)	return ret;
+
+	ret = 0;
+	if (x == n - 1 && y == n - 1)	return ret = 1;
+
+	for (int i = 0; i < 2; i++)
+	{
+		int nx = x + mv1[i] * maps[x][y];
+		int ny = y + mv2[i] * maps[x][y];
+
+		if (nx < 0 || ny < 0 || nx >= n || ny >= n)	continue;
+
+		ret += dfs(nx, ny);
+	}
+
+	return ret;
+}
+
+int main(void) {
+	FIO;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> maps[i][j];
+			dp[i][j] = -1;
+		}
+	}
+
+	dfs(0, 0);
+
+	cout << dp[0][0];
+}
