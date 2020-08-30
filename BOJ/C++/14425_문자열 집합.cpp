6c1b7d956d83570c48d75426d762acc9a60a72e6
leejaeseung <wasd222@naver.com>
2020-08-30 22:15:57 +0900

    싐싐

---
+++ BOJ/C++/14425_문자열 집합.cpp
@@ -0,0 +1,79 @@
+/*
+문제
+총 N개의 문자열로 이루어진 집합 S가 주어진다.
+
+입력으로 주어지는 M개의 문자열 중에서 집합 S에 포함되어 있는 것이 총 몇 개인지 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 문자열의 개수 N과 M (1 ≤ N ≤ 10,000, 1 ≤ M ≤ 10,000)이 주어진다.
+
+다음 N개의 줄에는 집합 S에 포함되어 있는 문자열들이 주어진다.
+
+다음 M개의 줄에는 검사해야 하는 문자열들이 주어진다.
+
+입력으로 주어지는 문자열은 알파벳 소문자로만 이루어져 있으며, 길이는 500을 넘지 않는다. 집합 S에 같은 문자열이 여러 번 주어지는 경우는 없다.
+
+출력
+첫째 줄에 M개의 문자열 중에 총 몇 개가 집합 S에 포함되어 있는지 출력한다.
+
+풀이:
+심심풀이
+*/
+#include<iostream>
+#include<memory.h>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+#define MOD 100007
+int mv1[4] = { 0, 0, 1, -1 };
+int mv2[4] = { 1, -1, 0, 0 };
+int mv_all1[8] = { 0, 1, 0, -1, -1, -1, 1, 1 };
+int mv_all2[8] = { 1, 0, -1, 0 , -1, 1, -1, 1 };
+
+int n, m;
+set<string> s;
+
+int main(void) {
+	FIO;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+
+		s.insert(str);
+	}
+
+	int ans = 0;
+
+	for (int i = 0; i < m; i++)
+	{
+		string str;
+		cin >> str;
+
+		if (s.find(str) != s.end())
+			ans++;
+	}
+
+	cout << ans;
+}
