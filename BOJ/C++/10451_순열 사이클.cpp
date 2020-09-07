6c7745369481fd74c742f32f1652f8cd21ad6038
leejaeseung <wasd222@naver.com>
2020-09-07 22:38:10 +0900

    그래프

---
+++ BOJ/C++/10451_순열 사이클.cpp
@@ -0,0 +1,88 @@
+/*
+문제
+
+
+1부터 N까지 정수 N개로 이루어진 순열을 나타내는 방법은 여러 가지가 있다. 예를 들어, 8개의 수로 이루어진 순열 (3, 2, 7, 8, 1, 4, 5, 6)을 배열을 이용해 표현하면  와 같다. 또는, Figure 1과 같이 방향 그래프로 나타낼 수도 있다.
+
+순열을 배열을 이용해  로 나타냈다면, i에서 πi로 간선을 이어 그래프로 만들 수 있다.
+
+Figure 1에 나와있는 것 처럼, 순열 그래프 (3, 2, 7, 8, 1, 4, 5, 6) 에는 총 3개의 사이클이 있다. 이러한 사이클을 "순열 사이클" 이라고 한다.
+
+N개의 정수로 이루어진 순열이 주어졌을 때, 순열 사이클의 개수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스의 첫째 줄에는 순열의 크기 N (2 ≤ N ≤ 1,000)이 주어진다. 둘째 줄에는 순열이 주어지며, 각 정수는 공백으로 구분되어 있다.
+
+출력
+각 테스트 케이스마다, 입력으로 주어진 순열에 존재하는 순열 사이클의 개수를 출력한다.
+
+풀이:
+간단한 그래프 탐색 문제이다. N^2 으로 풀이했다.
+*/
+#include<iostream>
+#include<memory.h>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+#define MOD 100003
+int mv1[4] = { 0, 0, 1, -1 };
+int mv2[4] = { 1, -1, 0, 0 };
+int mv_all1[8] = { 0, 1, 0, -1, -1, -1, 1, 1 };
+int mv_all2[8] = { 1, 0, -1, 0 , -1, 1, -1, 1 };
+
+
+int main(void) {
+	FIO;
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> vt(n + 1, 0);
+		vector<bool> visit(n + 1, false);
+
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> vt[i];
+		}
+
+		int ans = 0;
+
+		for (int i = 1; i <= n; i++)
+		{
+			int now = i;
+			if (!visit[now]) {
+				ans++;
+
+				while (!visit[now]) {
+					visit[now] = true;
+					now = vt[now];
+				}
+			}
+		}
+
+		cout << ans << "\n";
+	}
+}
