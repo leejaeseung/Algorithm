3cedc93f6fc152985477d20418f810ee6ab35af4
leejaeseung <wasd222@naver.com>
2020-09-21 15:16:34 +0900

    구현

---
+++ BOJ/C++/10996_별 찍기 - 21.cpp
@@ -0,0 +1,60 @@
+/*
+문제
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 차례대로 별을 출력한다.
+풀이:
+.
+*/
+#include<iostream>
+#include<memory.h>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+#define MOD 100003
+int mv1[4] = { 0, 0, 1, -1 };
+int mv2[4] = { 1, -1, 0, 0 };
+int mv_all1[8] = { 0, 1, 0, -1, -1, -1, 1, 1 };
+int mv_all2[8] = { 1, 0, -1, 0 , -1, 1, -1, 1 };
+
+int main(void) {
+	FIO;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n * 2; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if ((i + j) % 2 == 0)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << "\n";
+	}
+}
