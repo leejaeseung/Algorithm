192f3f61742e5f504de4266a0537969f0073a262
leejaeseung <wasd222@naver.com>
2021-02-03 23:54:05 +0900

    dp

---
+++ BOJ/C++/10164_격자상의 경로.cpp
@@ -0,0 +1,91 @@
+/*
+문제
+행의 수가 N이고 열의 수가 M인 격자의 각 칸에 1부터 N×M까지의 번호가 첫 행부터 시작하여 차례로 부여되어 있다. 격자의 어떤 칸은 ○ 표시가 되어 있다. (단, 1번 칸과 N × M번 칸은 ○ 표시가 되어 있지 않다. 또한, ○ 표시가 되어 있는 칸은 최대 한 개이다. 즉, ○ 표시가 된 칸이 없을 수도 있다.)
+
+행의 수가 3이고 열의 수가 5인 격자에서 각 칸에 번호가 1부터 차례대로 부여된 예가 아래에 있다. 이 격자에서는 8번 칸에 ○ 표시가 되어 있다.
+
+
+
+격자의 1번 칸에서 출발한 어떤 로봇이 아래의 두 조건을 만족하면서 N×M번 칸으로 가고자 한다.
+
+조건 1: 로봇은 한 번에 오른쪽에 인접한 칸 또는 아래에 인접한 칸으로만 이동할 수 있다. (즉, 대각선 방향으로는 이동할 수 없다.)
+조건 2: 격자에 ○로 표시된 칸이 있는 경우엔 로봇은 그 칸을 반드시 지나가야 한다.
+위에서 보인 것과 같은 격자가 주어질 때, 로봇이 이동할 수 있는 서로 다른 경로의 두 가지 예가 아래에 있다.
+
+1 → 2 → 3 → 8 → 9 → 10 → 15
+1 → 2 → 3 → 8 → 13 → 14 → 15
+격자에 관한 정보가 주어질 때 로봇이 앞에서 설명한 두 조건을 만족하면서 이동할 수 있는 서로 다른 경로가 총 몇 개나 되는지 찾는 프로그램을 작성하라.
+
+입력
+입력의 첫째 줄에는 격자의 행의 수와 열의 수를 나타내는 두 정수 N과 M(1 ≤ N, M ≤ 15), 그리고 ○로 표시된 칸의 번호를 나타내는 정수 K(K=0 또는 1 < K < N×M)가 차례로 주어지며, 각 값은 공백으로 구분된다. K의 값이 0인 경우도 있는데, 이는 ○로 표시된 칸이 없음을 의미한다. N과 M이 동시에 1인 경우는 없다.
+
+출력
+주어진 격자의 정보를 이용하여 설명한 조건을 만족하는 서로 다른 경로의 수를 계산하여 출력해야 한다.
+
+풀이:
+메모이제이션을 사용한 dfs로 풀이, O 점에 무조건 가야 하므로 두 번에 나눠 dfs한다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pic pair<int, char>
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vii vector<pii>
+#define IMAX 2000000001
+#define LMAX 1000000000000000000
+#define DMAX 0xFFFFFFFFFFFFF
+int mv1[4] = { 0, 1, -1, 0 };
+int mv2[4] = { 1, 0, 0, -1 };
+
+int n, m, k;
+int dp[16][16];
+
+int dfs(int x, int y, int ex, int ey) {
+
+	int& ret = dp[x][y];
+
+	if (x == ex && y == ey)	return 1;
+	if (ret != -1)	return ret;
+	ret = 0;
+
+	for (int i = 0; i < 2; i++)
+	{
+		int nx = x + mv1[i];
+		int ny = y + mv2[i];
+
+		if (nx > ex || ny > ey)	continue;
+
+		ret += dfs(nx, ny, ex, ey);
+	}
+
+	return ret;
+}
+
+int main() {
+
+	cin >> n >> m >> k;
+
+	fill(&dp[0][0], &dp[15][16], -1);
+
+	if (k == 0) {
+		cout << dfs(1, 1, n, m);
+	}
+	else {
+		int tx = (k - 1) / m + 1;
+		int ty = (k - 1) % m + 1;
+
+		cout << dfs(1, 1, tx, ty) * dfs(tx, ty, n, m);
+	}
+}
